ec1acf2afa1ff17448258c681f41ffdb04e6d6d4
Andrew Brown <andrew.brown@bom.gov.au>
2018-10-26 14:48:19 +1100

    Use wrf-python 3d_CAPE to calculate CAPE (vectorised)

--- calc_param.py
+++ calc_param.py
@@ -11,6 +11,10 @@ import datetime as dt
 import glob
 import pandas as pd
 import os
+from skewt import SkewT
+from metpy.units import units
+import metpy.calc as mpcalc
+import wrf
 
 
 #Functions to take reanalysis data at a single time step over some spatial domain, and calculate TS parameters
@@ -38,10 +42,141 @@ def get_point(point,lon,lat,ta,dp,hgt,ua,va,uas,vas):
 
 	return [ta,dp,hgt,ua,va,uas,vas]
 
-def calc_param(times,ta,dp,hur,hgt,p,ua,va,uas,vas,lon,lat,param,model,save):
+def calc_param_wrf(times,ta,dp,hur,hgt,terrain,p,ps,ua,va,uas,vas,lon,lat,param,model,save):
+	#Use 3d_cape in wrf-python to calculate MUCAPE (vectorised). Use this to calculate other
+	# params
+
+	#Input vars are of shape [time, levels, lat, lon]
+	#Output is a list of numpy arrays of length=len(params) with dimensions [time,lat,lon]
+	#Option to save as a netcdf file
+
+	#Assign p levels to a 4d array
+	p_3d = np.empty(ta.shape)
+	for i in np.arange(0,ta.shape[0]):
+		for j in np.arange(0,ta.shape[2]):
+			for k in np.arange(0,ta.shape[3]):
+				p_3d[i,:,j,k] = p
+
+	param = np.array(param)
+	param_out = [0] * (len(param))
+	for i in np.arange(0,len(param)):
+		param_out[i] = np.empty((len(times),len(lat),len(lon)))
+
+	for t in np.arange(0,len(times)):
+		print(times[t])
+		hur_unit = units.percent*hur[t,:,:,:]
+		ta_unit = units.degC*ta[t,:,:,:]
+		p_unit = units.hectopascals*p_3d[t,:,:,:]
+		q = mpcalc.mixing_ratio_from_relative_humidity(hur_unit,\
+		ta_unit,p_unit)
+		q = np.array(q)
+		cape = wrf.cape_3d(p_3d[t,:,:,:],ta[t,:,:,:]+273.15,q\
+			,hgt[t,:,:,:],terrain,ps[t,:,:],False,meta=False,missing=0)
+
+		if "relhum850-500" in param:
+			param_ind = np.where(param=="relhum850-500")[0][0]
+			param_out[param_ind][t,y,x] = \
+				np.mean(hur_p[(p_p<=851) & (p_p>=499)])
+		if "mu_cape" in param:
+		#CAPE for most unstable parcel
+			param_ind = np.where(param=="mu_cape")[0][0]
+			param_out[param_ind][t,:,:] = np.max(cape.data[0],axis=0)
+		if "s06" in param:
+		#Wind shear 10 m (sfc) to 6 km
+			ua_0km = uas[t]
+			va_0km = vas[t]
+			ua_6km = np.empty(ua_0km.shape)
+			va_6km = np.empty(va_0km.shape)
+			for i in np.arange(0,len(lat)):
+			    for j in np.arange(0,len(lon)):
+				ua_6km[i,j] = np.interp(6000,hgt[t,:,i,j],ua[t,:,i,j])
+				va_6km[i,j] = np.interp(6000,hgt[t,:,i,j],va[t,:,i,j])
+			shear = np.sqrt(np.square(ua_6km-ua_0km)+\
+					np.square(va_6km-va_0km))
+			param_ind = np.where(param=="s06")[0][0]
+			param_out[param_ind][t,:,:] = shear
+		if "mu_cin" in param:
+		#CIN for most unstable parcel
+			param_ind = np.where(param=="mu_cin")[0][0]
+			param_out[param_ind][t,y,x] = -1*mu_parcel.bminus
+		if "hel01" in param:
+		#Combined (+ve and -ve) rel. helicity from 0-1 km
+			param_ind = np.where(param=="hel01")[0][0]
+			param_out[param_ind][t,y,x] = winds.helicity(prof,0,1000)[0]
+		if "hel03" in param:
+		#Combined (+ve and -ve) rel. helicity from 0-3 km
+			param_ind = np.where(param=="hel03")[0][0]
+			param_out[param_ind][t,y,x] = winds.helicity(prof,0,3000)[0]
+		if "hel06" in param:
+		#Combined (+ve and -ve) rel. helicity from 0-6 km
+			param_ind = np.where(param=="hel06")[0][0]
+			param_out[param_ind][t,y,x] = winds.helicity(prof,0,6000)[0]
+		if "ship" in param:
+		#Significant hail parameter
+			param_ind = np.where(param=="ship")[0][0]
+			param_out[param_ind][t,y,x] = params.ship(prof,\
+				mupcl=mu_parcel)
+		if "lhp" in param:
+		#Large Hail Paramerer; NOTE requires convective profile (costly).
+			conf_prof = profile.create_profile(profile="convective",\
+				pres=p, hght=hgt_p, tmpc=ta_p, \
+				dwpc=dp_p, u=ua_p_kts, v=va_p_kts)
+			param_ind = np.where(param=="lhp")[0][0]
+			param_out[param_ind][t,y,x] = params.lhp(prof)
+		if "hgz_depth" in param:
+		#Hail growzth zone (in hPa)
+			param_ind = np.where(param=="hgz_depth")[0][0]
+			param_out[param_ind][t,y,x] = abs(params.hgz(prof)[1]\
+				 - params.hgz(prof)[0])
+		if "dcp" in param:
+		#Derecho Composite Parameter ~ cold pool driven wind events
+			param_ind = np.where(param=="dcp")[0][0]
+			param_out[param_ind][t,y,x] = params.dcp(prof)
+		if "mburst" in param:
+		#Microburst composite index
+			param_ind = np.where(param=="mburst")[0][0]
+			param_out[param_ind][t,y,x] = params.mburst(prof)
+		if "mmp" in param:
+		#Mesoscale Convective System Maintanance Probability
+			param_ind = np.where(param=="mmp")[0][0]
+			param_out[param_ind][t,y,x] = params.mmp(prof,\
+				mupcl=mu_parcel)
+
+	if save:
+		fname = "/g/data/eg3/ab4502/ExtremeWind/"+model+"_"+\
+			dt.datetime.strftime(times[0],"%Y%m%d")+"_"+\
+			dt.datetime.strftime(times[-1],"%Y%m%d")+".nc"
+		if os.path.isfile(fname):
+			os.remove(fname)
+		param_file = nc.Dataset(fname,"w")
+		time_dim = param_file.createDimension("time",len(times))
+		lat_dim = param_file.createDimension("lat",len(lat))
+		lon_dim = param_file.createDimension("lon",len(lon))
+
+		date_list_str = np.array([dt.datetime.strftime(x,"%Y-%m-%d %H:%M") \
+			for x in times])
+		time_var = param_file.createVariable("time",date_list_str.dtype,("time",))
+		lat_var = param_file.createVariable("lat",lat.dtype,("lat",))
+		lon_var = param_file.createVariable("lon",lat.dtype,("lon",))
+		time_var[:] = date_list_str
+		lat_var[:] = lat
+		lon_var[:] = lon
+
+		for i in np.arange(0,len(param)):
+			temp_var = param_file.createVariable(param[i],param_out[i].dtype,\
+				("time","lat","lon"))
+			temp_var[:] = param_out[i]
+		
+	return param_out
+
+
+def calc_param_sharppy(times,ta,dp,hur,hgt,p,ua,va,uas,vas,lon,lat,param,model,save):
+	#Calculate parameters based on the SHARPpy package for creating profiles
+
 	#For each time in "times", loop over lat/lon points in domain and calculate:
 	# 1) profile 2) parcel (if create_parcel is set) 3) parameter
 	#NOTE the choice of parameter may affect both steps 2) and 3)
+	#Input vars are of shape [time, levels, lat, lon]
 	#Output is a list of numpy arrays of length=len(params) with dimensions [time,lat,lon]
 	#Option to save as a netcdf file
 
@@ -51,41 +186,35 @@ def calc_param(times,ta,dp,hur,hgt,p,ua,va,uas,vas,lon,lat,param,model,save):
 		param_out[i] = np.empty((len(times),len(lat),len(lon)))
 
 	for t in np.arange(0,len(times)):
-		#print(times[t])
+		print(times[t])
 		for x in np.arange(0,len(lon)):
 			for y in np.arange(0,len(lat)):
 				#Restrict to a single spatial point
-				start=dt.datetime.now()
 				point = [lon[x],lat[y]]   
 				ta_p,dp_p,hgt_p,ua_p,va_p,uas_p,vas_p = get_point(point,\
 					lon,lat,ta[t,:,:,:],dp[t,:,:,:],hgt[t,:,:,:],ua[t,:,:,:],\
 					va[t,:,:,:],uas[t,:,:],vas[t,:,:])
-				print("POINT SLICING: "+ str(dt.datetime.now() - start))
+
 				#Convert u and v to kts for use in profile
 				ua_p_kts = utils.MS2KTS(ua_p)
 				va_p_kts = utils.MS2KTS(va_p)
+
 				#Create profile
-				start=dt.datetime.now()
 				prof = profile.create_profile(pres=p, hght=hgt_p, tmpc=ta_p, \
 						dwpc=dp_p, u=ua_p_kts, v=va_p_kts)
-				print("PROFILE" + str(dt.datetime.now() - start))
 				#Create most unstable parcel
-				start=dt.datetime.now()
 				mu_parcel = params.parcelx(prof, flag=3,dp=-10)
-				print("PARCEL" + str(dt.datetime.now() - start))
+
 				if "relhum850-500" in param:
 					param_ind = np.where(param=="relhum850-500")[0][0]
 					param_out[param_ind][t,y,x] = \
 						np.mean(hur_p[(p_p<=851) & (p_p>=499)])
 				if "mu_cape" in param:
 				#CAPE for most unstable parcel
-					start=dt.datetime.now()
 					param_ind = np.where(param=="mu_cape")[0][0]
 					param_out[param_ind][t,y,x] = mu_parcel.bplus
-					print("CAPE" + str(dt.datetime.now() - start))
 				if "s06" in param:
 				#Wind shear 10 m (sfc) to 6 km
-					start=dt.datetime.now()
 					ua_0km = uas_p
 					ua_6km = np.interp(6000,hgt_p,ua_p)
 					va_0km = vas_p
@@ -94,7 +223,6 @@ def calc_param(times,ta,dp,hur,hgt,p,ua,va,uas,vas,lon,lat,param,model,save):
 						np.square(va_6km-va_0km))
 					param_ind = np.where(param=="s06")[0][0]
 					param_out[param_ind][t,y,x] = shear
-					print("s06" + str(dt.datetime.now() - start))
 				if "mu_cin" in param:
 				#CIN for most unstable parcel
 					param_ind = np.where(param=="mu_cin")[0][0]
@@ -142,35 +270,7 @@ def calc_param(times,ta,dp,hur,hgt,p,ua,va,uas,vas,lon,lat,param,model,save):
 					param_out[param_ind][t,y,x] = params.mmp(prof,\
 						mupcl=mu_parcel)
 
-	if save:
-		fname = "/g/data/eg3/ab4502/ExtremeWind/"+model+"_"+\
-			dt.datetime.strftime(times[0],"%Y%m%d")+"_"+\
-			dt.datetime.strftime(times[-1],"%Y%m%d")+".nc"
-		if os.path.isfile(fname):
-			os.remove(fname)
-		param_file = nc.Dataset(fname,"w")
-		time_dim = param_file.createDimension("time",len(times))
-		lat_dim = param_file.createDimension("lat",len(lat))
-		lon_dim = param_file.createDimension("lon",len(lon))
-
-		date_list_str = np.array([dt.datetime.strftime(x,"%Y-%m-%d %H:%M") \
-			for x in times])
-		time_var = param_file.createVariable("time",date_list_str.dtype,("time",))
-		lat_var = param_file.createVariable("lat",lat.dtype,("lat",))
-		lon_var = param_file.createVariable("lon",lat.dtype,("lon",))
-		time_var[:] = date_list_str
-		lat_var[:] = lat
-		lon_var[:] = lon
-
-		for i in np.arange(0,len(param)):
-			temp_var = param_file.createVariable(param[i],param_out[i].dtype,\
-				("time","lat","lon"))
-			temp_var[:] = param_out[i]
-		
-	return param_out
-
-
-def calc_param_points(times,ta,dp,hur,hgt,p,ua,va,uas,vas,lon,lat,lon_used,lat_used,param,loc_id):
+def calc_param_points(times,ta,dp,hur,hgt,terrain,p,ps,ua,va,uas,vas,lon,lat,lon_used,lat_used,param,loc_id):
 
 	#Same as calc_param except from inputs generated by read_*model*_points()
 	#Variables are now of the form [time, pressure, points]
@@ -194,21 +294,39 @@ def calc_param_points(times,ta,dp,hur,hgt,p,ua,va,uas,vas,lon,lat,lon_used,lat_u
 			hgt_p = hgt[t,:,point]; ta_p = ta[t,:,point]; dp_p = dp[t,:,point];
 			hur_p = hur[t,:,point];	ua_p = ua[t,:,point]; va_p = va[t,:,point]; 
 			uas_p = uas[t,point]; vas_p = vas[t,point]; p_p = p[t,:,point]
+			ps_p = ps[t,point]
 
+			#------------------------------------------------------------------------
+			#USING SHARPpy
+			#------------------------------------------------------------------------
 			#Convert u and v to kts for use in profile
-			ua_p_kts = utils.MS2KTS(ua_p)
-			va_p_kts = utils.MS2KTS(va_p)
+			#ua_p_kts = utils.MS2KTS(ua_p)
+			#va_p_kts = utils.MS2KTS(va_p)
 			#Create profile
-			prof = profile.create_profile(pres=p_p, hght=hgt_p, tmpc=ta_p, \
-					dwpc=dp_p, u=ua_p_kts, v=va_p_kts)
+			#prof = profile.create_profile(pres=p_p, hght=hgt_p, tmpc=ta_p, \
+					#dwpc=dp_p, u=ua_p_kts, v=va_p_kts)
 			#Create most unstable parcel
-			mu_parcel = params.parcelx(prof, flag=3)
+			#mu_parcel = params.parcelx(prof, flag=3)
+
+			#------------------------------------------------------------------------
+			#With wrf-python (for erai)
+			#------------------------------------------------------------------------
+			hur_unit = units.percent*hur_p
+			ta_unit = units.degC*ta_p
+			p_unit = units.hectopascals*p_p
+			q = mpcalc.mixing_ratio_from_relative_humidity(hur_unit,ta_unit,p_unit)
+			q = np.array(q)
+			cape = wrf.cape_3d(p_p,ta_p+273.15,q\
+				,hgt_p,terrain[point],ps_p,False,meta=False,missing=0)
+
 			if "relhum850-500" in param:
 				values[cnt,param=="relhum850-500"] = \
 					np.mean(hur_p[(p_p<=851) & (p_p>=499)])
 			if "mu_cape" in param:
 			#CAPE for most unstable parcel
-				values[cnt,param=="mu_cape"] = mu_parcel.bplus
+				#values[cnt,param=="mu_cape"] = mu_parcel.bplus
+				#values[cnt,param=="mu_cape"] = cape.data[0][0][0]
+				values[cnt,param=="mu_cape"] = np.max(cape.data[0,:,0,0])
 			if "s06" in param:
 			#Wind shear 10 m (sfc) to 6 km
 				ua_0km = uas_p
--- erai_read.py
+++ erai_read.py
@@ -19,7 +19,8 @@ from calc_param import *
 from barra_read import date_seq
 
 def read_erai(domain,times):
-	#Open ERA-Interim netcdf files and extract variables needed for a range of times and given spatial domain
+	#Open ERA-Interim netcdf files and extract variables needed for a range of times 
+	# and given spatial domain
 	#Option to also use one time (include hour)
 
 	ref = dt.datetime(1900,1,1,0,0,0)
@@ -41,6 +42,7 @@ def read_erai(domain,times):
 	lat_ind = np.where((lat >= domain[0]) & (lat <= domain[1]))[0]
 	lon = lon[lon_ind]
 	lat = lat[lat_ind]
+	terrain = reform_terrain(lon,lat)
 
 	#Initialise arrays for each variable
 	ta = np.empty((len(date_list),no_p,len(lat_ind),len(lon_ind)))
@@ -51,6 +53,7 @@ def read_erai(domain,times):
 	va = np.empty((len(date_list),no_p,len(lat_ind),len(lon_ind)))
 	uas = np.empty((len(date_list),len(lat_ind),len(lon_ind)))
 	vas = np.empty((len(date_list),len(lat_ind),len(lon_ind)))
+	ps = np.empty((len(date_list),len(lat_ind),len(lon_ind)))
 
 	for date in unique_dates:
 		print(date)
@@ -70,6 +73,8 @@ hur_6hrs_ERAI_historical_an-pl_"+date+"*.nc")[0])
 uas_6hrs_ERAI_historical_an-sfc_"+date+"*.nc")[0])
 		vas_file = nc.Dataset(glob.glob("/g/data/ub4/erai/netcdf/6hr/atmos/oper_an_sfc/v01/vas/\
 vas_6hrs_ERAI_historical_an-sfc_"+date+"*.nc")[0])
+		ps_file = nc.Dataset(glob.glob("/g/data/ub4/erai/netcdf/6hr/atmos/oper_an_sfc/v01/ps/\
+ps_6hrs_ERAI_historical_an-sfc_"+date+"*.nc")[0])
 
 		#Get times to load in from file
 		times = ta_file["time"][:]
@@ -86,14 +91,16 @@ vas_6hrs_ERAI_historical_an-sfc_"+date+"*.nc")[0])
 		dp[date_ind,:,:,:] = get_dp(ta[date_ind,:,:,:],hur[date_ind,:,:,:])
 		uas[date_ind,:,:] = uas_file["uas"][time_ind,lat_ind,lon_ind]
 		vas[date_ind,:,:] = vas_file["vas"][time_ind,lat_ind,lon_ind]
+		ps[date_ind,:,:] = ps_file["ps"][time_ind,lat_ind,lon_ind] / 100
 
 		ta_file.close();z_file.close();ua_file.close();va_file.close();hur_file.close();uas_file.close();vas_file.close()
 	
-	return [ta,dp,hgt,p,ua,va,uas,vas,lon,lat,date_list]
+	return [ta,dp,hur,hgt,terrain,p,ps,ua,va,uas,vas,lon,lat,date_list]
 	
 def read_erai_points(points,times):
 
-	#Open ERA-Interim netcdf files and extract variables needed for a range of times at a given set of spatial points
+	#Open ERA-Interim netcdf files and extract variables needed for a range of 
+	# times at a given set of spatial points
 
 	#Format dates and times
 	ref = dt.datetime(1900,1,1,0,0,0)
@@ -108,6 +115,7 @@ def read_erai_points(points,times):
 	no_p, pres, p_ind = get_pressure(100)
 	lon,lat = get_lat_lon()
 	[lon_ind, lat_ind, lon_used, lat_used] = get_lat_lon_inds(points,lon,lat)
+	terrain_new = reform_terrain(lon,lat)
 
 	#Initialise arrays for each variable
 	ta = np.empty((len(date_list),no_p,len(points)))
@@ -119,6 +127,7 @@ def read_erai_points(points,times):
 	va = np.empty((len(date_list),no_p,len(points)))
 	uas = np.empty((len(date_list),len(points)))
 	vas = np.empty((len(date_list),len(points)))
+	ps = np.empty((len(date_list),len(points)))
 
 	for date in unique_dates:
 		print(date)
@@ -138,6 +147,8 @@ hur_6hrs_ERAI_historical_an-pl_"+date+"*.nc")[0])
 uas_6hrs_ERAI_historical_an-sfc_"+date+"*.nc")[0])
 		vas_file = nc.Dataset(glob.glob("/g/data/ub4/erai/netcdf/6hr/atmos/oper_an_sfc/v01/vas/\
 vas_6hrs_ERAI_historical_an-sfc_"+date+"*.nc")[0])
+		ps_file = nc.Dataset(glob.glob("/g/data/ub4/erai/netcdf/6hr/atmos/oper_an_sfc/v01/ps/\
+ps_6hrs_ERAI_historical_an-sfc_"+date+"*.nc")[0])
 
 		#Get times to load in from file
 		times = ta_file["time"][:]
@@ -146,27 +157,38 @@ vas_6hrs_ERAI_historical_an-sfc_"+date+"*.nc")[0])
 
 		#Load data for each spatial point given to function
 		for point in np.arange(0,len(points)):
-			ta[date_ind,:,point] = ta_file["ta"][time_ind,p_ind,lat_ind,lon_ind] - 273.15
-			ua[date_ind,:,point] = ua_file["ua"][time_ind,p_ind,lat_ind,lon_ind]
-			va[date_ind,:,point] = va_file["va"][time_ind,p_ind,lat_ind,lon_ind]
-			hgt[date_ind,:,point] = z_file["z"][time_ind,p_ind,lat_ind,lon_ind] / 9.8
-			hur[date_ind,:,point] = hur_file["hur"][time_ind,p_ind,lat_ind,lon_ind]
+			ta[date_ind,:,point] = ta_file["ta"][time_ind,p_ind,lat_ind[point]\
+						,lon_ind[point]] - 273.15
+			ua[date_ind,:,point] = ua_file["ua"][time_ind,p_ind,lat_ind[point]\
+						,lon_ind[point]]
+			va[date_ind,:,point] = va_file["va"][time_ind,p_ind,lat_ind[point]\
+						,lon_ind[point]]
+			hgt[date_ind,:,point] = z_file["z"][time_ind,p_ind,lat_ind[point]\
+						,lon_ind[point]] / 9.8
+			hur[date_ind,:,point] = hur_file["hur"][time_ind,p_ind,lat_ind[point]\
+						,lon_ind[point]]
 			hur[hur<0] = 0
 			dp[date_ind,:,point] = get_dp(ta[date_ind,:,point],hur[date_ind,:,point])
-			uas[date_ind,point] = uas_file["uas"][time_ind,lat_ind,lon_ind]
-			vas[date_ind,point] = vas_file["vas"][time_ind,lat_ind,lon_ind]
+			uas[date_ind,point] = uas_file["uas"][time_ind,lat_ind[point]\
+						,lon_ind[point]]
+			vas[date_ind,point] = vas_file["vas"][time_ind,lat_ind[point]\
+						,lon_ind[point]]
+			ps[date_ind,point] = ps_file["ps"][time_ind,lat_ind[point]\
+						,lon_ind[point]]
 			p[date_ind,:,point] = pres[p_ind]
 
-		ta_file.close();z_file.close();ua_file.close();va_file.close();hur_file.close();uas_file.close();vas_file.close()
+		ta_file.close();z_file.close();ua_file.close();va_file.close();hur_file.close();uas_file.close();vas_file.close();ps_file.close()
 
 	#Save lat/lon as array
 	lon = np.empty((len(points)))
 	lat = np.empty((len(points)))
+	terrain = np.empty((len(points)))
 	for point in np.arange(0,len(points)):
 		lon[point] = points[point][0]
 		lat[point] = points[point][1]
+		terrain[point] = terrain_new[lat_ind[point],lon_ind[point]]
 	
-	return [ta,dp,hur,hgt,p,ua,va,uas,vas,lon,lat,lon_used,lat_used,date_list]
+	return [ta,dp,hur,hgt,terrain,p,ps,ua,va,uas,vas,lon,lat,lon_used,lat_used,date_list]
 
 def get_pressure(top):
 	#Returns [no of levels, levels, indices below "top"]
@@ -174,16 +196,20 @@ def get_pressure(top):
 ta_6hrs_ERAI_historical_an-pl_"+"201201"+"*.nc")[0])
 	p =ta_file["lev"][:]/100
 	p_ind = np.where(p>=top)[0]
+	ta_file.close()
 	return [len(p_ind), p, p_ind]
 
 def get_lsm():
+	#Load the ERA-Interim land-sea mask (land = 1)
 	lsm_file = nc.Dataset("/short/eg3/ab4502/erai_lsm.nc")
 	lsm = np.squeeze(lsm_file.variables["lsm"][:])
 	lsm_lon = np.squeeze(lsm_file.variables["longitude"][:])
 	lsm_lat = np.squeeze(lsm_file.variables["latitude"][:])
+	lsm_file.close()
 	return [lsm,lsm_lon,lsm_lat]
 
 def reform_lsm(lon,lat):
+	#Re-shape the land sea mask to go from longitude:[0,360] to [-180,180]
 	[lsm,lsm_lon,lsm_lat] = get_lsm()
 	lsm_new = np.empty(lsm.shape)
 
@@ -193,6 +219,26 @@ def reform_lsm(lon,lat):
 			lsm_new[i,j] = lsm[lat[i]==lsm_lat, lon[j]==lsm_lon]
 	return lsm_new
 
+def get_terrain():
+	#Load the ERA-Interim surface geopetential height as terrain height
+	terrain_file = nc.Dataset("/short/eg3/ab4502/erai_sfc_geopt.nc")
+	terrain = np.squeeze(terrain_file.variables["z"][:])/9.8
+	terrain_lon = np.squeeze(terrain_file.variables["longitude"][:])
+	terrain_lat = np.squeeze(terrain_file.variables["latitude"][:])
+	terrain_file.close()
+	return [terrain,terrain_lon,terrain_lat]
+
+def reform_terrain(lon,lat):
+	#Re-shape terrain height to go from longitude:[0,360] to [-180,180]
+	[terrain,terrain_lon,terrain_lat] = get_terrain()
+	terrain_new = np.empty((len(lat),len(lon)))
+
+	terrain_lon[terrain_lon>=180] = terrain_lon[terrain_lon>=180]-360
+	for i in np.arange(0,len(lat)):
+		for j in np.arange(0,len(lon)):
+			terrain_new[i,j] = terrain[lat[i]==terrain_lat, lon[j]==terrain_lon]
+	return terrain_new
+
 def format_dates(x):
 	return dt.datetime.strftime(x,"%Y") + dt.datetime.strftime(x,"%m")
 
@@ -201,6 +247,7 @@ def get_lat_lon():
 ta_6hrs_ERAI_historical_an-pl_"+"201201"+"*.nc")[0])
 	lon = ta_file["lon"][:]
 	lat = ta_file["lat"][:]
+	ta_file.close()
 	return [lon,lat]
 
 def get_lat_lon_inds(points,lon,lat):

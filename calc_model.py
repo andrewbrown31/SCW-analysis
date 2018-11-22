f49ce9ab6372b0a121353af878d9be4c98549d63
Andrew Brown <andrew.brown@bom.gov.au>
2018-11-23 10:34:01 +1100

    Included barra driver and extra parameters

--- calc_model.py
+++ calc_model.py
@@ -82,6 +82,55 @@ def calc_model(model,out_name,method,time,param,issave,region,cape_method):
 			dt.datetime.strftime(times[0],"%Y%m%d")+"_"+\
 			dt.datetime.strftime(times[1],"%Y%m%d")+".csv",float_format="%.3f")	
 
+def barra_driver():
+	#Drive calc_model() for each month available in the BARRA dataset, for the sa_small domain
+
+	model = "barra"
+	cape_method = "wrf"
+	method = "domain"
+	region = "sa_small"
+	param = ["ml_cape","ml_cin","mu_cin","mu_cape","srh01","srh03","srh06","scp",\
+		"stp","ship","mmp","relhum850-500","crt","non_sc_stp","vo","lr1000","lcl",\
+		"relhum1000-700","ssfc6","ssfc3","ssfc1","s06","ssfc850","ssfc500",\
+		"cape*s06","cape*ssfc6"]
+	dates = []
+	for y in [2010,2011,2012,2013,2014,2015]:
+		for m in [1,2,3,4,5,6,7,8,9,10,11,12]:
+		    if (m != 12):
+			dates.append([dt.datetime(y,m,1,0,0,0),\
+				dt.datetime(y,m+1,1,0,0,0)-dt.timedelta(hours = 6)])
+		    else:
+			dates.append([dt.datetime(y,m,1,0,0,0),\
+				dt.datetime(y+1,1,1,0,0,0)-dt.timedelta(hours = 6)])
+	dates[0][0] = dt.datetime(2010,1,1,6,0,0)
+	for t in np.arange(0,len(dates)):
+		print(str(dates[t][0])+" - "+str(dates[t][1]))
+		calc_model(model,model,method,dates[t],param,True,region,cape_method)	
+	
+def erai_driver():
+	#Drive calc_model() for 2010-2015 in ERA-Interim, for the sa_small domain
+
+	model = "erai"
+	cape_method = "wrf"
+	method = "domain"
+	region = "sa_small"
+	param = ["ml_cape","ml_cin","mu_cin","mu_cape","srh01","srh03","srh06","scp",\
+		"stp","ship","mmp","relhum850-500","crt","non_sc_stp","vo","lr1000","lcl",\
+		"relhum1000-700","ssfc6","ssfc3","ssfc1","s06","ssfc850","ssfc500",\
+		"cape*s06","cape*ssfc6"]
+	dates = []
+	for y in np.arange(2005,2010):
+		for m in [1,2,3,4,5,6,7,8,9,10,11,12]:
+		    if (m != 12):
+			dates.append([dt.datetime(y,m,1,0,0,0),\
+				dt.datetime(y,m+1,1,0,0,0)-dt.timedelta(hours = 6)])
+		    else:
+			dates.append([dt.datetime(y,m,1,0,0,0),\
+				dt.datetime(y+1,1,1,0,0,0)-dt.timedelta(hours = 6)])
+	for t in np.arange(0,len(dates)):
+		print(str(dates[t][0])+" - "+str(dates[t][1]))
+		calc_model(model,model,method,dates[t],param,True,region,cape_method)	
+
 if __name__ == "__main__":
 
 #--------------------------------------------------------------------------------------------------
@@ -89,18 +138,24 @@ if __name__ == "__main__":
 #--------------------------------------------------------------------------------------------------
 
 	model = "erai"
-	cape_method = "points_wrf"
+	cape_method = "wrf"
 
-	method = "points"
-	region = "adelaideAP"
+	method = "domain"
+	region = "sa_small"
 
 	experiment = ""
 
 	#ADELAIDE = UTC + 10:30
 	#times = [dt.datetime(2010,1,1,6,0,0),dt.datetime(2015,12,31,0,0,0)]
-	times = [dt.datetime(2014,11,22,0,0,0),dt.datetime(2014,11,23,0,0,0)]
+	times = [dt.datetime(2010,1,1,6,0,0),dt.datetime(2010,1,1,6,0,0)]
 		#[dt.datetime(2014,9,28,0,0,0),dt.datetime(2014,9,29,0,0,0)]]
 	issave = True
+
+	loc_id = ["Port Augusta","Marree","Munkora","Woomera","Robe","Loxton","Coonawarra","Renmark",\
+			"Clare HS","Adelaide AP"]
+	points = [(137.78,-32.54),(138.0684,-29.6587),(140.3273,-36.1058),(138.82,-31.15),\
+			(139.8054,-37.1776),(140.5978,-34.439),(140.8254,-37.2906),\
+			(140.6766,-34.1983),(138.5933,-33.8226),(138.53,-34.96)]
 
 #--------------------------------------------------------------------------------------------------
 #	RUN CODE
@@ -111,16 +166,22 @@ if __name__ == "__main__":
 	out_name = model+"_"+cape_method+experiment
 
 	if cape_method == "wrf":
-		param = ["ml_cape","ml_cin","mu_cin","mu_cape","s06","srh01","srh03","srh06","scp",\
-			"stp","ship","mmp","relhum850-500","crt","non_sc_stp","vo","lr1000","lcl"]
+		param = ["ml_cape","ml_cin","mu_cin","mu_cape","srh01","srh03","srh06","scp",\
+			"stp","ship","mmp","relhum850-500","crt","non_sc_stp","vo","lr1000","lcl",\
+			"relhum1000-700","ssfc6","ssfc3","ssfc1","s06","ssfc850","ssfc500",\
+			"cape*s06","cape*ssfc6"]
 	elif cape_method == "SHARPpy":
-		param = ["mu_cin","mu_cape","s06","s0850","srh01","srh03","srh06","scp",\
+		param = ["mu_cin","mu_cape","s06","ssfc850","srh01","srh03","srh06","scp",\
 			"ship","mmp","relhum850-500","stp"]
 	elif cape_method == "points_wrf":
-		param = ["ml_cape","ml_cin","mu_cin","mu_cape","s06","srh01","srh03","srh06","scp",\
-			"stp","ship","mmp","relhum850-500","crt","lr1000","lcl","cape*s06"]
+		param = ["ml_cape","ml_cin","mu_cin","mu_cape","srh01","srh03","srh06","scp",\
+			"stp","ship","mmp","relhum850-500","crt","lr1000","lcl",\
+			"relhum1000-700","ssfc6","ssfc3","ssfc1","s06","ssfc850","ssfc500",\
+			"cape*s06","cape*ssfc6"]
 	elif cape_method == "points_SHARPpy":
 		param = ["mu_cape","s06","cape*s06"]	
 
 	#for time in times:
-	calc_model(model,out_name,method,times,param,issave,region,cape_method)	
+	#calc_model(model,out_name,method,times,param,issave,region,cape_method)	
+	#barra_driver()
+	erai_driver()

88b4a9582c8baacb753fa2f87388321c6578457f
Andrew Brown <andrew.brown@bom.gov.au>
2019-06-13 08:37:52 +1000

    Function arguments

--- calc_barra.py
+++ calc_barra.py
@@ -5,26 +5,11 @@
 from calc_model import *
 from multiprocessing import Pool
 
-def barra_par_driver(date):
-	#Drive calc_model() for each month available in the BARRA dataset, for the sa_small domain
-
-	param = ["ml_cape","ml_cin","mu_cin","mu_cape","srh01","srh03","srh06","scp",\
-		"stp","ship","mmp","relhum850-500","vo10","lr1000","lcl",\
-		"relhum1000-700","s06","s0500","s01","s03",\
-		"cape*s06","dcp","td850","td800","td950","dcape","mlm","dlm",\
-		"dcape*cs6","mlm+dcape","mlm*dcape*cs6"]
-	model = "barra"
-	cape_method = "wrf"
-	method = "domain"
-	region = "aus"
-	calc_model(model,model,method,date,param,True,region,cape_method)	
-
 if __name__ == "__main__":
 
-	pool = Pool()
 	dates = []
 	for y in np.arange(2003,2017):
-		for m in [1,2,3,4,5,6]:
+		for m in [1,2,3,4,5,6,7,8,9,10,11,12]:
 		    if (m != 12):
 			dates.append([dt.datetime(y,m,1,0,0,0),\
 				dt.datetime(y,m+1,1,0,0,0)-dt.timedelta(hours = 6)])
@@ -32,5 +17,17 @@ if __name__ == "__main__":
 			dates.append([dt.datetime(y,m,1,0,0,0),\
 				dt.datetime(y+1,1,1,0,0,0)-dt.timedelta(hours = 6)])
 	dates = np.array(dates)
-	pool.map(barra_par_driver, dates)
 
+	param = ["ml_cape","ml_cin","mu_cin","mu_cape","srh01","srh03","srh06","scp",\
+		"stp","ship","mmp","relhum850-500","vo10","lr1000","lcl",\
+		"relhum1000-700","s06","s0500","s01","s03",\
+		"cape*s06","dcp","td850","td800","td950","dcape","mlm","dlm",\
+		"dcape*cs6","mlm+dcape","mlm*dcape*cs6","mf","sf","cond"]
+	model = "barra"
+	cape_method = "wrf_par"
+	method = "domain"
+	region = "sa_small"
+
+	for date in dates:
+		[param,param_out,lon,lat,date_list] = \
+			calc_model(model,model,method,date,param,True,region,cape_method)	

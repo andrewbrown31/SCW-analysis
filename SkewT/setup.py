385f76d5cee86221e22a760d3b3e13dbfbdaa359
Andrew Brown <ab4502@gadi-login-02.gadi.nci.org.au>
2020-04-23 10:54:26 +1000

    SkewT

---
+++ SkewT/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='SkewT',
+    version='1.2.0',
+    author='Timothy Lang',
+    author_email='timothy.j.lang@nasa.gov',
+    packages=['', 'skewt'],
+    scripts=[],
+    license='LICENSE.txt',
+    description='Plots & analyses atmospheric profile data from UWyo database',
+    long_description=open('README.rst').read(),
+)

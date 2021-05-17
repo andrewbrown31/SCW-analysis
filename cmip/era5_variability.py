962a848493842bf74c58c750b9c00fe99a811a77
Andrew Brown <andrew.brown@bom.gov.au>
2021-05-17 14:42:30 +1000

    Added RSTAT back to plot

--- cmip/era5_variability.py
+++ cmip/era5_variability.py
@@ -35,7 +35,7 @@ era5_aws.loc[:,"logit_sta"] = logit.fit(era5_sta[era5_sta_preds], era5_sta["is_s
 
 #Plot results for the DCP
 fig=plt.figure(figsize=[10,12])
-plt.subplot(4,2,1)
+plt.subplot(5,2,1)
 v="dcp"
 thresh=0.15
 y1=[]; y2=[] 
@@ -52,7 +52,7 @@ plt.gca().set_xticks(np.arange(1,13,1))
 plt.gca().set_xticklabels("")
 plt.text(0.025, 0.1, 'a)', horizontalalignment='left',verticalalignment='center',transform=plt.gca().transAxes, size=12)
 
-plt.subplot(4,2,2)
+plt.subplot(5,2,2)
 r, p = get_corr(y1,y2)
 plt.title(("r={:.3f} ({:.3f})").format(r,p))
 plt.plot(np.arange(2005,2019), y1, label=v, color=c1,marker="o");plt.gca().grid(axis="both"); plt.gca().tick_params(axis="y",labelcolor=c1);ax2=plt.gca().twinx(); ax2.plot(np.arange(2005,2019), y2, color=c2, label="SCW events",marker="o")
@@ -61,7 +61,7 @@ plt.text(0.025, 0.1, 'b)', horizontalalignment='left',verticalalignment='center'
 plt.gca().set_xticklabels("")
 
 #Plot results for SHERBE
-plt.subplot(4,2,3)
+plt.subplot(5,2,3)
 v="eff_sherb"
 thresh=0.47
 y1=[]; y2=[] 
@@ -77,7 +77,7 @@ plt.title("SHERBE")
 plt.gca().set_xticklabels("")
 plt.gca().set_xticks(np.arange(1,13,1))
 plt.text(0.025, 0.1, 'c)', horizontalalignment='left',verticalalignment='center',transform=plt.gca().transAxes, size=12)
-plt.subplot(4,2,4)
+plt.subplot(5,2,4)
 r, p = get_corr(y1,y2)
 plt.title(("r={:.3f} ({:.3f})").format(r,p))
 plt.plot(np.arange(2005,2019), y1, label=v, color=c1,marker="o");plt.gca().grid(axis="both"); plt.gca().tick_params(axis="y",labelcolor=c1);ax2=plt.gca().twinx(); ax2.plot(np.arange(2005,2019), y2, color=c2, label="SCW events",marker="o")
@@ -86,7 +86,7 @@ plt.text(0.025, 0.1, 'd)', horizontalalignment='left',verticalalignment='center'
 plt.gca().set_xticklabels("")
 
 #Plot results for T-Totals
-plt.subplot(4,2,5)
+plt.subplot(5,2,5)
 v="t_totals"
 thresh=48.1
 y1=[]; y2=[] 
@@ -102,7 +102,7 @@ plt.title("T-Totals")
 plt.gca().set_xticks(np.arange(1,13,1))
 plt.gca().set_xticklabels("")
 plt.text(0.025, 0.1, 'e)', horizontalalignment='left',verticalalignment='center',transform=plt.gca().transAxes, size=12)
-plt.subplot(4,2,6)
+plt.subplot(5,2,6)
 r, p = get_corr(y1,y2)
 plt.title(("r={:.3f} ({:.3f})").format(r,p))
 plt.plot(np.arange(2005,2019), y1, label=v, color=c1,marker="o");plt.gca().grid(axis="both"); plt.gca().tick_params(axis="y",labelcolor=c1);ax2=plt.gca().twinx(); ax2.plot(np.arange(2005,2019), y2, color=c2, label="SCW events",marker="o")
@@ -111,7 +111,7 @@ plt.text(0.025, 0.1, 'f)', horizontalalignment='left',verticalalignment='center'
 plt.gca().set_xticklabels("")
 
 #Plot results for MSTAT
-plt.subplot(4,2,7)
+plt.subplot(5,2,7)
 v="logit"
 thresh=0.83
 y1=[]; y2=[]
@@ -125,42 +125,42 @@ for m in np.arange(1,13):
 plt.plot(np.arange(1,13), m1, color=c1, marker="o"); plt.gca().grid(axis="both");plt.gca().tick_params(axis="y",labelcolor=c1); ax2=plt.gca().twinx(); ax2.plot(np.arange(1,13), m2, color=c2,marker="o")
 plt.title("BDSD")
 plt.gca().set_xticks(np.arange(1,13,1))
-plt.gca().set_xticklabels(["J","F","M","A","M","J","J","A","S","O","N","D"])
+plt.gca().set_xticklabels("")
 plt.text(0.025, 0.1, 'g)', horizontalalignment='left',verticalalignment='center',transform=plt.gca().transAxes, size=12)
-plt.subplot(4,2,8)
+plt.subplot(5,2,8)
 ax1=plt.gca()
 r, p = get_corr(y1,y2)
 plt.title(("r={:.3f} ({:.3f})").format(r,p))
 plt.plot(np.arange(2005,2019), y1, label=v, color=c1,marker="o");plt.gca().grid(axis="both"); plt.gca().tick_params(axis="y",labelcolor=c1);ax2=plt.gca().twinx(); ax2.plot(np.arange(2005,2019), y2, color=c2, label="SCW events",marker="o")
 ax2.xaxis.set_major_locator(MaxNLocator(integer=True))
 plt.text(0.025, 0.1, 'h)', horizontalalignment='left',verticalalignment='center',transform=plt.gca().transAxes, size=12)
-#plt.gca().set_xticklabels("")
+plt.gca().set_xticklabels("")
 
 #Plot results for RSTAT
-#plt.subplot(5,2,9)
-#plt.xlabel("Month")
-#v="logit_sta"
-#thresh=0.77
-#y1=[]; y2=[]
-#for y in np.arange(2005,2019): 
- #        y1.append((era5_aws[(era5_aws.year==y)][v]>=thresh).sum()) 
-  #       y2.append(era5_aws[(era5_aws.year==y)]["is_conv_aws"].sum()) 
-#m1=[]; m2=[] 
-#for m in np.arange(1,13): 
-#         m1.append((era5_aws[(era5_aws.month==m)][v] >= thresh).sum()) 
-#         m2.append(era5_aws[(era5_aws.month==m)]["is_conv_aws"].sum()) 
-#plt.plot(np.arange(1,13), m1, color=c1, marker="o"); plt.gca().grid(axis="both");plt.gca().tick_params(axis="y",labelcolor=c1); ax2=plt.gca().twinx(); ax2.plot(np.arange(1,13), m2, color=c2,marker="o")
-#plt.title("RSTAT")
-#plt.gca().set_xticks(np.arange(2,14,2))
-#plt.text(0.025, 0.1, 'i)', horizontalalignment='left',verticalalignment='center',transform=plt.gca().transAxes, size=12)
-#plt.subplot(5,2,10)
-#ax1=plt.gca()
-#plt.xlabel("Year")
-#r, p = get_corr(y1,y2)
-#plt.title(("r={:.3f} ({:.3f})").format(r,p))
-#plt.plot(np.arange(2005,2019), y1, label=v, color=c1,marker="o");plt.gca().grid(axis="both"); plt.gca().tick_params(axis="y",labelcolor=c1);ax2=plt.gca().twinx(); ax2.plot(np.arange(2005,2019), y2, color=c2, label="SCW events",marker="o")
-#ax2.xaxis.set_major_locator(MaxNLocator(integer=True))
-#plt.text(0.025, 0.1, 'j)', horizontalalignment='left',verticalalignment='center',transform=plt.gca().transAxes, size=12)
+plt.subplot(5,2,9)
+plt.xlabel("Month")
+v="logit_sta"
+thresh=0.72 #0.77
+y1=[]; y2=[]
+for y in np.arange(2005,2019): 
+         y1.append((era5_aws[(era5_aws.year==y)][v]>=thresh).sum()) 
+         y2.append(era5_aws[(era5_aws.year==y)]["is_conv_aws"].sum()) 
+m1=[]; m2=[] 
+for m in np.arange(1,13): 
+         m1.append((era5_aws[(era5_aws.month==m)][v] >= thresh).sum()) 
+         m2.append(era5_aws[(era5_aws.month==m)]["is_conv_aws"].sum()) 
+plt.plot(np.arange(1,13), m1, color=c1, marker="o"); plt.gca().grid(axis="both");plt.gca().tick_params(axis="y",labelcolor=c1); ax2=plt.gca().twinx(); ax2.plot(np.arange(1,13), m2, color=c2,marker="o")
+plt.title("BDSD-R")
+plt.gca().set_xticks(np.arange(2,14,2))
+plt.text(0.025, 0.1, 'i)', horizontalalignment='left',verticalalignment='center',transform=plt.gca().transAxes, size=12)
+plt.subplot(5,2,10)
+ax1=plt.gca()
+plt.xlabel("Year")
+r, p = get_corr(y1,y2)
+plt.title(("r={:.3f} ({:.3f})").format(r,p))
+plt.plot(np.arange(2005,2019), y1, label=v, color=c1,marker="o");plt.gca().grid(axis="both"); plt.gca().tick_params(axis="y",labelcolor=c1);ax2=plt.gca().twinx(); ax2.plot(np.arange(2005,2019), y2, color=c2, label="SCW events",marker="o")
+ax2.xaxis.set_major_locator(MaxNLocator(integer=True))
+plt.text(0.025, 0.1, 'j)', horizontalalignment='left',verticalalignment='center',transform=plt.gca().transAxes, size=12)
 
 ax2.plot([0,0],[1,0], color="tab:blue", label="Diagnostic");
 ax2.set_xlim([2005,2018])
